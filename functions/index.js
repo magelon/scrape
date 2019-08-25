0f4f4447738834a20b58555c5c5f8af1325bcd13
magelon <alfardproject@gmail.com>
2019-08-25 07:34:18 -0700

    c

--- functions/index.js
+++ functions/index.js
@@ -1,4 +1,8 @@
 const functions = require('firebase-functions');
+const cors=require('cors')({origin:true});
+const cheerio =require('cheerio');
+const getUrls=require('get-urls');
+const fetch=require('node-fetch');
 
 // // Create and Deploy Your First Cloud Functions
 // // https://firebase.google.com/docs/functions/write-firebase-functions
@@ -6,3 +10,36 @@ const functions = require('firebase-functions');
 // exports.helloWorld = functions.https.onRequest((request, response) => {
 //  response.send("Hello from Firebase!");
 // });
+
+const scrapeMetatags=(text)=>{
+    const url=Array.from(getUrls(text));
+    const requests=urls.map(async url=>{
+        const res=await fetch(url);
+        const html=await res.text();
+        const $=cheerio.load(html);
+
+        const getMetatag=(name)=>
+        $(`meta[name=${name}]`).attr('content')||
+        $(`meta[property="og:${name}"]`).attr('content')||
+        $(`meta[property="wtitter:${name}"]`).attr('content');
+
+        return{
+            url,
+            title:$('title').first().text(),
+            favicon:$('link[rel="shortcut icon"]').attr('href'),
+            // description:$('meta[name=description]').attr('content'),
+            description:getMetatag('description'),
+            image:getMetatag('image'),
+            author:getMetatag('author'),
+        }
+    });
+    return Promise.all(requests);
+}
+
+exports.scraper=functions.https.onRequest((request,response)=>{
+    cors(request,response,async()=>{
+        const body=JSON.parse(request.body);
+        const data=await scrapeMetatags(body.text);
+        response.send(data)
+    });
+});
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HttpClient, HttpParams, HttpHeaders } from '@angular/common/http';
+
 
 @Component({
   selector: 'app-root',
@@ -7,4 +9,34 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'scrape';
+
+  constructor(private http: HttpClient) {
+    
+  }
+
+
+  scraper(tag){
+  
+    let search={
+      tag:tag
+    }
+  
+    let header=new HttpHeaders({
+      'Content-Type' : 'application/json',
+      'Cache-Control': 'no-cache',
+      'Access-Control-Allow-Origin': '*',
+      'Access-Control-Allow-Headers': "*",
+    });
+    
+    let options={headers:header};
+    let Root_url2= 'http://localhost:5001'
+      this.http.post(Root_url2+'scraper',search,options).toPromise().then(data=>
+        {
+          console.log(data);
+        }).catch(err=>{
+          console.log(err);
+        });
+  
+  }
+
 }
