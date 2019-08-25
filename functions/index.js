fb012ebaa8c2c63f0b57104f84f80c38e6c5f166
magelon <alfardproject@gmail.com>
2019-08-25 09:08:17 -0700

    c

--- functions/index.js
+++ functions/index.js
@@ -3,6 +3,7 @@ const cors=require('cors')({origin:true});
 const cheerio =require('cheerio');
 const getUrls=require('get-urls');
 const fetch=require('node-fetch');
+const puppeteer=require('puppeteer');
 
 // // Create and Deploy Your First Cloud Functions
 // // https://firebase.google.com/docs/functions/write-firebase-functions
@@ -10,6 +11,29 @@ const fetch=require('node-fetch');
 // exports.helloWorld = functions.https.onRequest((request, response) => {
 //  response.send("Hello from Firebase!");
 // });
+
+const scrapeImages=async(link)=>{
+    const browser=await puppeteer.launch({
+               headless: true,
+               args: ['--no-sandbox', '--disable-setuid-sandbox']});
+    const page=await browser.newPage();
+    await page.goto(link);
+    await page.waitForSelector('img',{
+        visible:true,
+    })
+
+    await page.screenshot({path:'1.png', fullPage: true})
+
+    const data=await page.evaluate(()=>{
+        const images=document.querySelectorAll('img');
+        const urls=Array.from(images).map(v=>v.src);
+        return urls
+    });
+
+    await browser.close();
+    console.log(data);
+    return data;
+}
 
 const scrapeMetatags=(text)=>{
     const urls=Array.from(getUrls(text));
@@ -40,7 +64,8 @@ exports.scraper=functions.https.onRequest((request,response)=>{
     cors(request,response,async()=>{
         console.log(request.body.text)
         // const body=JSON.parse(request.body);
-         const data=await scrapeMetatags(request.body.text);
+        //  const data=await scrapeMetatags(request.body.text);
+        const data= await scrapeImages(request.body.text);
         response.send(data)
     });
 });
